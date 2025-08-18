34ab7b0e76f898ad0f72433020191bfb81045646
sarathy <thesarath75@gmail.com>
2025-08-18 13:52:34 +0530

    Best time to sell and buy  stack problem solved!

--- DSA_Problems/Arrays/BestTime_To_Buy_Sell_stack.java
+++ DSA_Problems/Arrays/BestTime_To_Buy_Sell_stack.java
@@ -2,8 +2,49 @@ package Arrays;
 
 public class BestTime_To_Buy_Sell_stack {
 
+	
+	//Naive Solution
+	public static int findMaximumProfit(int[] prices) {
+		int n = prices.length;
+		int maxProfit = 0;
+		for(int i=0 ;i < n; i++ ) {
+			for(int j = i+1; j < n; j++) {
+			  int profit = prices[j] - prices[i];
+			  if(profit > maxProfit)
+				  maxProfit = profit;
+			}
+		}
+		
+		return maxProfit;
+	}
+	
+	//Optimized solution
+	public static int findMaximumProfitOpt(int[] prices) {
+		int n = prices.length;
+		int maxProfit = 0;
+		int minimumPrice = Integer.MAX_VALUE;
+		for(int i=0 ;i < n; i++ ) {
+			if(prices[i] < minimumPrice) {
+				minimumPrice = prices[i];
+			}
+			else if(prices[i] - minimumPrice > maxProfit) {
+				maxProfit = prices[i] - minimumPrice;
+			}
+		}
+				
+		return maxProfit;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int[] prices = {7,1,5,3,6,4};
+		int maxProfit = findMaximumProfit(prices);
+		System.out.println(maxProfit);
+		
+		
+		int maxProfitOpt = findMaximumProfitOpt(prices);
+		System.out.println(maxProfitOpt);
+		
+		
 
 	}
 
